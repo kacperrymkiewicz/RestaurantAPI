6545ddcd8e6642fa915428395dd49bfa99e55132
Kacper <kacperrymkiewicz@gmail.com>
2025-04-08 17:28:04 +0200

    Create api app, add serializers, change media path

---
+++ restaurant_project/api/serializers.py
@@ -0,0 +1,65 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from restaurant_project.restaurant.models import Category, Ingredient, Product, OrderItem, Order
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name', 'is_allergen']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(), source='category', write_only=True
+    )
+    ingredients = IngredientSerializer(read_only=True, many=True)
+    ingredient_ids = serializers.PrimaryKeyRelatedField(
+        queryset=Ingredient.objects.all(), many=True, source='ingredients', write_only=True
+    )
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'price', 'image',
+                  'category', 'category_id', 'ingredients', 'ingredient_ids']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    product_id = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(), source='product', write_only=True
+    )
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'product_id', 'quantity']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'status', 'created_at', 'items']
+
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')
+        order = Order.objects.create(user=self.context['request'].user, **validated_data)
+        for item_data in items_data:
+            OrderItem.objects.create(order=order, **item_data)
+        return order
